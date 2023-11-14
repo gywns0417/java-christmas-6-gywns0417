d2a245d27ddfd0e6b016481378a1f4793fc606d3
gywns0417 <gywns0417@gmail.com>
2023-11-14 21:45:34 +0900

    refactor: OrderService 클래스에서 OrderMenu 클래스 분리
    OrderService 클래스에서 검증 완료된 주문 목록을 가지는 OrderMenu 클래스를 분리했습니다.

---
+++ src/main/java/christmas/domain/order/OrderMenu.java
@@ -0,0 +1,49 @@
+package christmas.domain.order;
+
+import static christmas.config.ErrorMessage.MENU_QUANTITY_INPUT_ERROR_MESSAGE;
+import static christmas.config.EventConfig.MENU_TOTAL_QUANTITY_MAX;
+
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class OrderMenu {
+    private static final String orderMenuRegex = "^([가-힣]+-[1-9]\\d*(,\\s*[가-힣]+-[1-9]\\d*)*)$";
+
+    private final HashMap<String, Integer> orderMenuQuantity;
+
+    public OrderMenu(String inputOrderMenuQuantity) {
+        String validaOrderMenu = validateRegexOrderMenu(inputOrderMenuQuantity);
+        this.orderMenuQuantity = splitOrderMenu(validaOrderMenu);
+        validateMenuQuantities();
+    }
+
+    private String validateRegexOrderMenu(String inputOrderMenu) {
+        if (!inputOrderMenu.matches(orderMenuRegex)) {
+            throw new IllegalArgumentException(MENU_QUANTITY_INPUT_ERROR_MESSAGE.getMessage());
+        }
+        return inputOrderMenu;
+    }
+
+    private void validateMenuQuantities() {
+        boolean isOverMenuQuantityMax = orderMenuQuantity.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum() > MENU_TOTAL_QUANTITY_MAX.getValue();
+        if (isOverMenuQuantityMax) {
+            throw new IllegalArgumentException(MENU_QUANTITY_INPUT_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    private HashMap<String, Integer> splitOrderMenu(String inputOrderMenu) {
+        HashMap<String, Integer> splitMenuQuantity = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(inputOrderMenu, ",-");
+
+        while (st.hasMoreTokens()) {
+            splitMenuQuantity.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+        }
+        return splitMenuQuantity;
+    }
+
+    public HashMap<String, Integer> getOrderMenuQuantity() {
+        return orderMenuQuantity;
+    }
+}
--- src/main/java/christmas/service/OrderService.java
+++ src/main/java/christmas/service/OrderService.java
@@ -8,81 +8,51 @@ import christmas.domain.menu.strategy.DrinkStrategy;
 import christmas.domain.menu.strategy.MainDishStrategy;
 import christmas.domain.menu.strategy.MenuStrategy;
 import christmas.domain.order.Order;
+import christmas.domain.order.OrderMenu;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.StringTokenizer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class OrderService { //TODO: 분리 고려
-    private static final String orderMenuRegex = "^([가-힣]+-[1-9]\\d*(,\\s*[가-힣]+-[1-9]\\d*)*)$";
-    private final List<MenuStrategy> strategies = List.of(
-            new AppetizerStrategy(),
-            new DessertStrategy(),
-            new DrinkStrategy(),
-            new MainDishStrategy()
-    );
-
+public class OrderService {
+    private final List<MenuStrategy> strategies;
     private final HashMap<String, Integer> appetizers = new HashMap<>();
     private final HashMap<String, Integer> dessert = new HashMap<>();
     private final HashMap<String, Integer> drink = new HashMap<>();
     private final HashMap<String, Integer> mainDish = new HashMap<>();
-    public Order start(Supplier<String> inputSupplier, Runnable messagePrinter, Consumer<String> errorMessagePrinter) {
+
+    public OrderService(List<MenuStrategy> strategies) {
+        this.strategies = strategies;
+    }
+    public Order order(Supplier<String> inputSupplier, Runnable messagePrinter, Consumer<String> errorMessagePrinter) {
         while (true) {
             try {
                 messagePrinter.run();
-                return createOrder(inputSupplier.get());
+                OrderMenu orderMenu = new OrderMenu(inputSupplier.get());
+                return createOrder(orderMenu, orderMenu.getOrderMenuQuantity(), strategies);
             } catch (IllegalArgumentException e) {
                 errorMessagePrinter.accept(e.getMessage());
             }
         }
     }
 
-    public Order createOrder(String inputMenu) {
-        String inputOrderMenu = validateRegexOrderMenu(inputMenu);
-        HashMap<String, Integer> splitMenuQuantity = splitOrderMenu(inputOrderMenu);
-        for (Entry<String, Integer> menuQuantity : splitMenuQuantity.entrySet()) {
+    private Order createOrder(OrderMenu orderMenu, HashMap<String, Integer> orderMenuQuantity, List<MenuStrategy> strategies) {
+        for (Entry<String, Integer> menuQuantity : orderMenuQuantity.entrySet()) {
             String menuName = menuQuantity.getKey();
             int quantity = menuQuantity.getValue();
 
-            MenuStrategy strategy = findStrategy(menuName); // 전략 검색
+            MenuStrategy strategy = findStrategy(menuName, strategies); // 전략 검색
             HashMap<String, Integer> menuMap = getMenuMap(strategy);
             if (menuMap.containsKey(menuName)) {
                 throw new IllegalArgumentException(MENU_QUANTITY_INPUT_ERROR_MESSAGE.getMessage());
             }
             strategy.putMenu(getMenuMap(strategy),menuName ,quantity); // 전략에 따라 해당하는 MenuMap에 put
         }
-        if (appetizers.isEmpty() && dessert.isEmpty() && mainDish.isEmpty()) {
-            throw new IllegalArgumentException(MENU_QUANTITY_INPUT_ERROR_MESSAGE.getMessage());
-        }
-        return new Order(splitMenuQuantity, appetizers, dessert, drink, mainDish);
+        return new Order(orderMenu, appetizers, dessert, drink, mainDish);
     }
 
-    private String validateRegexOrderMenu(String inputOrderMenu) {
-        if (!inputOrderMenu.matches(orderMenuRegex)) {
-            throw new IllegalArgumentException(MENU_QUANTITY_INPUT_ERROR_MESSAGE.getMessage());
-        }
-        return inputOrderMenu;
-    }
-
-    private HashMap<String, Integer> splitOrderMenu(String inputOrderMenu) {
-        HashMap<String, Integer> splitMenuQuantity = new HashMap<>();
-        StringTokenizer st = new StringTokenizer(inputOrderMenu, ",-");
-        int menuAmount = 0;
-        while (st.hasMoreTokens()) {
-            String menuName = st.nextToken();
-            int amount = Integer.parseInt(st.nextToken());
-            menuAmount += amount;
-            if(menuAmount > 20) {
-                throw new IllegalArgumentException(MENU_QUANTITY_INPUT_ERROR_MESSAGE.getMessage());
-            }
-            splitMenuQuantity.put(menuName, amount);
-        }
-        return splitMenuQuantity;
-    }
-
-    private MenuStrategy findStrategy(String menuName) {
+    private MenuStrategy findStrategy(String menuName, List<MenuStrategy> strategies) {
         return strategies.stream()
                 .filter(strategy -> strategy.acceptMenuName(menuName))
                 .findFirst()
