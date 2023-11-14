ec5015d73a2b3a03918060b001099abc575fc84c
gywns0417 <gywns0417@gmail.com>
2023-11-14 14:48:54 +0900

    feat: [사용자에게서 입력 받기] 기능 구현 완료

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView { //TODO: 안내 메시지 여기서 출력?
+    public static String getUserInput() {
+        return Console.readLine();
+    }
+}
