7d8310bb02712556182677a94ba672918333cf2b
gywns0417 <gywns0417@gmail.com>
2023-11-15 01:19:58 +0900

    style: 형식에 따른 enum 네이밍 수정
    FOR_GIVEAWAY_MIN_AMOUNT -> FOR_GIVEAWAY_AMOUNT_MIN로 수정했습니다.

--- src/main/java/christmas/domain/discount/strategy/GiveawayStrategy.java
+++ src/main/java/christmas/domain/discount/strategy/GiveawayStrategy.java
@@ -1,6 +1,6 @@
 package christmas.domain.discount.strategy;
 
-import static christmas.config.DiscountConfig.FOR_GIVEAWAY_MIN_AMOUNT;
+import static christmas.config.DiscountConfig.FOR_GIVEAWAY_AMOUNT_MIN;
 import static christmas.config.DiscountConfig.GIVEAWAY_DISCOUNT_AMOUNT;
 
 import christmas.domain.discount.DiscountContext;
@@ -9,7 +9,7 @@ import christmas.domain.discount.DiscountType;
 public class GiveawayStrategy implements DiscountStrategy {
     @Override
     public int accept(DiscountContext context) {
-        if (context.getTotalPurchaseAmount() >= FOR_GIVEAWAY_MIN_AMOUNT.getAmount()) {
+        if (context.getTotalPurchaseAmount() >= FOR_GIVEAWAY_AMOUNT_MIN.getAmount()) {
             return applyDiscount(context);
         }
         return 0;
