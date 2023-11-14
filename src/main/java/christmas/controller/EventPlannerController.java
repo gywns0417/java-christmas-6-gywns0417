f0af1e81f031148046b25077177d018754b5952a
gywns0417 <gywns0417@gmail.com>
2023-11-15 00:32:50 +0900

    refactor: 컨트롤러의 도메인 객체 접근 DTO로 수정
    컨트롤러에서 불필요하게 도메인 객체를 생성하는 부분을 DTO를 생성하도록 수정했습니다.

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -9,6 +9,10 @@ import christmas.domain.menu.strategy.DrinkStrategy;
 import christmas.domain.menu.strategy.MainDishStrategy;
 import christmas.domain.menu.strategy.MenuStrategy;
 import christmas.domain.order.Order;
+import christmas.dto.DiscountDto;
+import christmas.dto.OrderDto;
+import christmas.dto.TotalDiscountDto;
+import christmas.dto.VisitDateDto;
 import christmas.service.DateService;
 import christmas.service.DiscountService;
 import christmas.service.OrderService;
@@ -20,6 +24,7 @@ import java.util.List;
 
 
 public class EventPlannerController {
+    //TODO : 금액에 콤마 넣기
     List<MenuStrategy> strategies = List.of(new AppetizerStrategy(), new DessertStrategy(),
             new DrinkStrategy(), new MainDishStrategy());
     //TODO: 의존성 주입 구현해야함
@@ -29,14 +34,13 @@ public class EventPlannerController {
 
     public void run() {
         MessageOutputView.printWelcomeMessage();
-        VisitDate visitDate = dateService.getDateInput(InputView::getUserInput, MessageOutputView::printDateInputRequestMessage,
-                MessageOutputView::printErrorMessage);
-        Order order = orderService.order(InputView::getUserInput, MessageOutputView::printMenuQuantityInputRequestMessage,
-                MessageOutputView::printErrorMessage);
-        DiscountService discountService = new DiscountService(visitDate.toDto(), order.toDto());
-        Discount discount = discountService.createDiscount();
-        TotalDiscount totalDiscount = totalDiscountService.createTotalDiscount(discount.toDto(), order.toDto());
-        ResultOutputView.printResult(visitDate.toDto(), order.toDto(), discount.toDto(), totalDiscount.toDto());
-
+        VisitDateDto visitDateDto = dateService.getDateInput(InputView::getUserInput, MessageOutputView::printDateInputRequestMessage,
+                MessageOutputView::printErrorMessage).toDto();
+        OrderDto orderDto = orderService.order(InputView::getUserInput, MessageOutputView::printMenuQuantityInputRequestMessage,
+                MessageOutputView::printErrorMessage).toDto();
+        DiscountService discountService = new DiscountService(visitDateDto, orderDto);
+        DiscountDto discountDto = discountService.createDiscount().toDto();
+        TotalDiscountDto totalDiscountDto = totalDiscountService.createTotalDiscount(discountDto, orderDto).toDto();
+        ResultOutputView.printResult(visitDateDto, orderDto, discountDto, totalDiscountDto);
     }
 }
