cb2919ae410672307b921379b50ec2167b1e83e1
gywns0417 <gywns0417@gmail.com>
2023-11-14 21:56:29 +0900

    refactor: 컨트롤러 리팩토링
    이전의 변경점들에 맞춰 컨트롤러를 리팩토링했습니다.

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -2,27 +2,41 @@ package christmas.controller;
 
 import christmas.domain.date.VisitDate;
 import christmas.domain.discount.Discount;
+import christmas.domain.discount.TotalDiscount;
+import christmas.domain.menu.strategy.AppetizerStrategy;
+import christmas.domain.menu.strategy.DessertStrategy;
+import christmas.domain.menu.strategy.DrinkStrategy;
+import christmas.domain.menu.strategy.MainDishStrategy;
+import christmas.domain.menu.strategy.MenuStrategy;
 import christmas.domain.order.Order;
 import christmas.service.DateService;
 import christmas.service.DiscountService;
 import christmas.service.OrderService;
+import christmas.service.TotalDiscountService;
 import christmas.view.InputView;
-import christmas.view.OutputView;
+import christmas.view.MessageOutputView;
+import christmas.view.ResultOutputView;
+import java.util.List;
 
 
 public class EventPlannerController {
-    OrderService orderService = new OrderService();
+    List<MenuStrategy> strategies = List.of(new AppetizerStrategy(), new DessertStrategy(),
+            new DrinkStrategy(), new MainDishStrategy());
+    //TODO: 의존성 주입 구현해야함
+    OrderService orderService = new OrderService(strategies);
     DateService dateService = new DateService();
+    TotalDiscountService totalDiscountService = new TotalDiscountService();
 
     public void run() {
-        OutputView.printWelcomeMessage();
-        VisitDate visitDate = dateService.getDateInput(InputView::getUserInput, OutputView::printDateInputRequestMessage,
-                OutputView::printErrorMessage);
-        Order order = orderService.start(InputView::getUserInput, OutputView::printMenuQuantityInputRequestMessage,
-                OutputView::printErrorMessage);
+        MessageOutputView.printWelcomeMessage();
+        VisitDate visitDate = dateService.getDateInput(InputView::getUserInput, MessageOutputView::printDateInputRequestMessage,
+                MessageOutputView::printErrorMessage);
+        Order order = orderService.order(InputView::getUserInput, MessageOutputView::printMenuQuantityInputRequestMessage,
+                MessageOutputView::printErrorMessage);
         DiscountService discountService = new DiscountService(visitDate.toDto(), order.toDto());
         Discount discount = discountService.createDiscount();
-        OutputView.printResult(discount.toDto(), visitDate.toDto(), order.toDto());
+        TotalDiscount totalDiscount = totalDiscountService.createTotalDiscount(discount.toDto(), order.toDto());
+        ResultOutputView.printResult(visitDate.toDto(), order.toDto(), discount.toDto(), totalDiscount.toDto());
 
     }
 }
